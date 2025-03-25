8cf08ec386bcc9d18cb7f03a0550fc745dec94fe
Sandra9438Z <delblondsandra@gmail.com>
2025-03-25 12:00:40 +0100

    1er com

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,32 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RecettesComponent } from './pages/recettes/recettes.component';
+import { IngredientListComponent } from './shared/ingredient-list/ingredient-list.component';
+import { IngredientFormComponent } from './shared/ingredient-form/ingredient-form.component';
+import { IngredientImportExportComponent } from './shared/ingredient-import-export/ingredient-import-export.component';
+import { ModalBoxConfirmationComponent } from './shared/modal-box-confirmation/modal-box-confirmation.component';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { IngredientManagerPageComponent } from './pages/ingredient-manager-page/ingredient-manager-page.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    RecettesComponent
+    RecettesComponent,
+    IngredientListComponent,
+    IngredientFormComponent,
+    IngredientImportExportComponent,
+    IngredientManagerPageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
